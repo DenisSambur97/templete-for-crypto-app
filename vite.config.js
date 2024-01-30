24b6f2518753be8f9457ac63fb78a3600c15bd27
Denis <denissand1997@gmail.com>
2024-01-30 16:55:09 +0300

    App is Done: v.1.1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ViteEnvPlugin from 'vite-plugin-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteEnvPlugin(),],
+  plugins: [react()],
 })
